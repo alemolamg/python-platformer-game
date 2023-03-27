1a66db45928bcd04b7bb8b49f698dc14409cd007
Alemol <alemolamg@outlook.com>
2023-03-27 18:56:45 +0200

    Fondo dibujado

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ pygame.init()
 
 pygame.display.set_caption("Platformer")
 
-BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 1000, 800  # Medidas de la pantalla
 FPS = 60
 PLAYER_VEL = 5
@@ -18,21 +17,28 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 def get_background(name):
-    image = pygame.image.load("assets", "Background", name)
+    image = pygame.image.load(join("assets", "Background", name))
     x, y, width, height = image.get_rect()
     tiles = []
 
     for i in range(WIDTH // width + 1):
-        for j in range(HEIGHT // height):
-            pos = [i * with, j * height]
+        for j in range(HEIGHT // height + 1):
+            pos = (i * width, j * height)
             tiles.append(pos)
 
-    return tiles, image 
+    return tiles, image
 
+
+def draw(window, background, bg_image):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    pygame.display.update()
 
 
 def main(window):
     clock = pygame.time.Clock()
+    backgroud, bg_image = get_background("Blue.png")  # TODO: Aquí podemos cambiar el color
 
     run = True
     while run:
@@ -43,6 +49,7 @@ def main(window):
                 run = False
                 break
 
+        draw(window, backgroud, bg_image)
 
 
 if __name__ == "__main__":
